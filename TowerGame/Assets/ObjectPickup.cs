9122181f869c4277948cd9b1790a2bd4d8be2014
Jatin Craine <jatin.craine@gmail.com>
2023-07-31 09:11:37 -0400

    changed picked up items to be kinematic instead of not affected by gravity

--- TowerGame/Assets/ObjectPickup.cs
+++ TowerGame/Assets/ObjectPickup.cs
@@ -6,17 +6,7 @@ public class ObjectPickup : MonoBehaviour
 {
     public Camera Camera;
     public Transform handLocation;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerStay(Collider other)
     {
         Ray ray = Camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
@@ -25,7 +15,7 @@ public class ObjectPickup : MonoBehaviour
         {
             other.transform.parent = handLocation;
             other.gameObject.GetComponent<BoxCollider>().isTrigger = true;
-            other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
             other.attachedRigidbody.velocity = new Vector3(0, 0, 0);
             other.transform.localPosition = new Vector3(0, 0, 0);
         }
