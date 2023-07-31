b224ef7d4ccee723898ab59b7768b75562cb8c31
Jatin Craine <jatin.craine@gmail.com>
2023-07-31 10:50:38 -0400

    modified pickup script

--- TowerGame/Assets/Scripts/ObjectPickup.cs
+++ TowerGame/Assets/Scripts/ObjectPickup.cs
@@ -6,20 +6,48 @@ public class ObjectPickup : MonoBehaviour
 {
     public Camera Camera;
     public Transform handLocation;
+    bool holdingObj = false;
+    int reachDist = 4;
+    Ray ray;
+    RaycastHit hitInfo;
 
-
+    private void Start()
+    {
+        ray = Camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+    }
     private void Update()
     {
-        Ray ray = Camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-        RaycastHit hitInfo;
-        if (Input.GetKeyDown(KeyCode.E) && Physics.Raycast(ray, out hitInfo, 4) && hitInfo.transform.gameObject.tag == "Pickup")
+        PickupCheck();
+        if (holdingObj)
+        {
+            PutDownCheck();
+        }
+    }
+
+    void PickupCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && Physics.Raycast(ray, out hitInfo, reachDist) && hitInfo.transform.gameObject.tag == "Pickup")
         {
             hitInfo.transform.parent = handLocation;
             hitInfo.collider.isTrigger = true;
             hitInfo.rigidbody.isKinematic = true;
             hitInfo.rigidbody.velocity = new Vector3(0, 0, 0);
             hitInfo.transform.localPosition = new Vector3(0, 0, 0);
+            holdingObj = true;
         }
     }
 
+    void PutDownCheck()
+    {
+        //int maxDist = Mathf.Max(reachDist, )
+        //RaycastHit hit;
+        //if (holdingObj && Input.GetKeyDown(KeyCode.E) && Physics.Raycast(ray, out hit, reachDist))
+        //{
+        //    holdingObj = false;
+        //    hitInfo.transform.position = new Vector3(0, reachDist * Mathf.Cos(Camera.transform.localEulerAngles.y), reachDist * Mathf.Sin(Camera.transform.localEulerAngles.y));
+        //    hitInfo.transform.parent = null;
+        //    hitInfo.collider.isTrigger = false;
+        //    hitInfo.rigidbody.isKinematic = false;
+        //}
+    }
 }
