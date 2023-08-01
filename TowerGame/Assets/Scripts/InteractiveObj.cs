5d307c34074704fec4b0136d711fd28957e1dc0e
Jatin Craine <jatin.craine@gmail.com>
2023-08-01 14:31:14 -0400

    Moved InteractiveObj script into the scripts folder.

--- TowerGame/Assets/Scripts/InteractiveObj.cs
+++ TowerGame/Assets/Scripts/InteractiveObj.cs
@@ -5,15 +5,4 @@ using UnityEngine;
 public class InteractiveObj : MonoBehaviour
 {
     public bool isActive;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
