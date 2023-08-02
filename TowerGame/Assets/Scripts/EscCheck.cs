8fd3d84c681c6291ee5a1ea6231ccf8d4fb72346
Jatin Craine <jatin.craine@gmail.com>
2023-08-02 10:11:26 -0400

    Added title screen button code and esc key to go to title and also deleted an unused script.

---
+++ TowerGame/Assets/Scripts/EscCheck.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EscCheck : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+}
